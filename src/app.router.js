1fa97a625642c66610ff482102d143ed32b53bf9
HalaMadi <Halamadi75@gmail.com>
2025-04-13 22:47:21 +0300

    implement database connection and update mongoose version

--- src/app.router.js
+++ src/app.router.js
@@ -1,7 +1,11 @@
+import dotenv from 'dotenv'
 import cors from 'cors'
+import connectDB from '../DB/connection.js';
 const initApp = async (app, express) => {
     app.use(express.json());
     app.use(cors());
+    dotenv.config()
+    connectDB()
     app.get('/', (req, res) => {
         return res.status(200).json({ message: 'Welcome...' })
     })
